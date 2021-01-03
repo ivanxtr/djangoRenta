68965ada50f76336d4d27985dbd3ebb45581abfa
Jorge Rodriguez <jorge.rodriguez@SAO-MC-201007D.local>
2021-01-03 15:44:27 -0600

    adding new config

--- Rest/settings.py
+++ Rest/settings.py
@@ -140,8 +140,6 @@ USE_TZ = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
