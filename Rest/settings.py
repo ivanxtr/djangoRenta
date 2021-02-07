fbfa0012c3da11fecc32edbea07aec257e67c183
Ivan Rodriguez <sbr.com@hotmail.com>
2021-02-07 11:48:15 -0600

    fix config

--- Rest/settings.py
+++ Rest/settings.py
@@ -145,7 +145,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8001",
     "http://127.0.0.1:9000",
-    "bodegas-react-ssr.herokuapp.com"
+    "https://bodegas-react-ssr.herokuapp.com"
 ]
 
 # Static files (CSS, JavaScript, Images)
