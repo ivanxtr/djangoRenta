f41c2df769ce8510efc52ee3843e075604738b70
Ivan Rodriguez <sbr.com@hotmail.com>
2021-02-13 12:28:17 -0600

    more changes

--- Bodegas/models.py
+++ Bodegas/models.py
@@ -50,7 +50,7 @@ class Listing(models.Model):
   photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
   photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
   photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-  aws = models.CharField(max_length=200)
+  aws = models.CharField(max_length=200, blank=True)
   list_date = models.DateTimeField(default=datetime.now, blank=True)
   def __str__(self):
     return self.title
